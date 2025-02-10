0689b649fdbcb81abe2c2e1fe66dec17ffb155f6
bilalhussain02 <syed.hussain@georgebrown.ca>
2025-02-09 22:24:50 -0500

    Assignment 1 commit

--- labs/functions_lab05.py
+++ labs/functions_lab05.py
@@ -1,7 +1,6 @@
 ﻿# Import the random library to use for the dice later
 import random
 
-
 # Will the line below print when you import function.py into main.py?
 # print("Inside function.py")
 
@@ -15,10 +14,10 @@ def use_loot(belt, health_points):
     first_item = belt.pop(0)
     if first_item in good_loot_options:
         health_points = min(20, (health_points + 2))
-        print("    |    You used " + first_item +" to up your health to " + str(health_points))
+        print("    |    You used " + first_item +" to up your health to " + str(health_points) )
     elif first_item in bad_loot_options:
         health_points = min(20, (health_points - 2))
-        print("    |    You used " + first_item + " to hurt your health to " + str(health_points))
+        print("    |    You used " +  first_item+ " to hurt your health to " + str(health_points) )
     else:
         print("    |    You used " + first_item + " but it's not helpful")
     return belt, health_points
@@ -42,11 +41,13 @@ def collect_loot(loot_options, belt):
               @@@@@@@@@@@@          
               """
     print(ascii_image3)
-    loot_roll = random.choice(range(1, len(loot_options) + 1))
-    loot = loot_options.pop(loot_roll - 1)
+    loot_roll= random.choice(range(1, len(loot_options) + 1))
+    loot = loot_options.pop(loot_roll -1)
     belt.append(loot)
-    print("     |    Your belt is: ", belt)
+    print("    |    Your belt: ", belt)
     return loot_options, belt
+
+
 
 
 # Hero's Attack Function
@@ -115,23 +116,25 @@ def monster_attacks(m_combat_strength, health_points):
         print("    |    The monster has reduced Player's health to: " + str(health_points))
     return health_points
 
-
 # Lab 5: Question 7
 # Recursion
 # You can choose to go crazy, but it will reduce your health points by 5
-def inception_dream():
+def inception_dream(num_dream_lvls):
+    num_dream_lvls = int(num_dream_lvls)
     # Base Case
+    if num_dream_lvls == 1:
+        print("    |    You are in the deepest dream level now")
+        print("    |", end="    ")
+        input("Start to go back to real life? (Press Enter)")
+        print("    |    You start to regress back through your dreams to real life.")
+        return 2
 
-    print("    |    You are in the deepest dream level now")
-    print("    |", end="    ")
-    input("Start to go back to real life? (Press Enter)")
-    print("    |    You start to regress back through your dreams to real life.")
-
-# Recursive Case
-
-# inception_dream(5)
-# 1 + inception_dream(4)
-# 1 + 1 + inception_dream(3)
-# 1 + 1 + 1 + inception_dream(2)
-# 1 + 1 + 1 + 1 + inception_dream(1)
-# 1 + 1 + 1 + 1 + 2
+    # Recursive Case
+    else:
+        # inception_dream(5)
+        # 1 + inception_dream(4)
+        # 1 + 1 + inception_dream(3)
+        # 1 + 1 + 1 + inception_dream(2)
+        # 1 + 1 + 1 + 1 + inception_dream(1)
+        # 1 + 1 + 1 + 1 + 2
+        return 1 + int(inception_dream(num_dream_lvls - 1))
